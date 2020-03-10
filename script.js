c22cc9b6cdc2d1207060f8d4a206c5603cd86e00
Max <mshachikov@mail.ru>
2020-03-10 21:13:21 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/*alert('hello World');*/
+/*let answer=+prompt('18 есть?', "да");
+console.log(typeof(answer));
+
+console.log('arr'+ ' - abj');
+console.log(4+ +" - jbj");*/
+let iner=10,
+    deer=10;
+console.log(iner++);
+console.log(deer--);
